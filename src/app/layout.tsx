a8dfbc1f6ce3f9e80674a8538b1fec146e145651
Nathan Simpson <wsimpson966@gmail.com>
2025-09-21 04:36:07 +0000

    can you just add a little margin and padding for the interactive campus

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -47,7 +47,7 @@ export default function RootLayout({
             <AppSidebar />
             <main className="flex flex-1 flex-col">
               <AppHeader />
-              <div className={cn("flex-1 overflow-y-auto", !isMapPage && "p-4 md:p-8")}>
+              <div className="flex-1 overflow-y-auto p-4 md:p-8">
                 {children}
               </div>
             </main>
